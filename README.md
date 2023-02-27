0413faaa27f679e6e3a189e550307d772462b300
Philippe Mourey <philippe@mourey.com>
2023-02-27 21:13:36 +0100

    sample app for job follow-up

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# jobs
+Pour faire joujou en ligne...
+- http://philrg.pythonanywhere.com/
+- http://pmourey.pythonanywhere.com/
+
+Pour bien démarrer:
+- https://www.kaherecode.com/tutorial/demarrer-avec-flask-un-micro-framework-python
+- https://pythonbasics.org/flask-sqlalchemy/
+- https://fr.w3docs.com/snippets/html/comment-ajouter-une-bordure-a-un-tableau-html.html
+- https://flask-sqlalchemy.palletsprojects.com/en/3.0.x/config/
+
+### Setup instructions
+- Installer les fichiers suivants sur la racine de l'application Flask créé sur Pythonanywhere:
+  - requirements.txt (liste des modules requis pour le fonctionnement de SQLAlchemy et Flask)
+  - flask_app.py (code de lancement de l'application Flask)
+  - templates/new.html (template Jinja2)
+  - templates/show_all.html (template Jinja2)
+  
+  - J'ai eu une erreur sur la console Bash du provider, lors de l'installation d'un module (problème de dépendance):
+  
+  `ERROR: pip's dependency resolver does not currently take into account all the packages that are installed. This behaviour is the source of the following dependency conflicts.
+  gevent 21.12.0 requires greenlet<2.0,>=1.1.0; platform_python_implementation == "CPython", but you have greenlet 2.0.1 which is incompatible.`
+
+    Je l'avais déjà eu sur mon IMac. Il faut upgrader la version de pip par la commande suivante, et réinstaller les modules pour faire disparaître le message:
+
+    `python -m pip install --upgrade pip
+
+    `pip install -r requirements.txt`
+
+    Et c'est aussi simple que cela!
---
+++ flask_app.py
@@ -0,0 +1,68 @@
+from enum import Enum
+
+from flask import Flask, request, flash, url_for, redirect, render_template
+
+# https://pypi.org/project/flask/FLAS-sqlalchemy/
+from flask_sqlalchemy import SQLAlchemy
+
+# SQLite
+# sqlite3_db = '/Users/display/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db'
+# uri: str = f'sqlite:///{sqlite3_db}'
+# MySQL
+# hostname: str = 'pmourey.mysql.pythonanywhere-services.com'
+# uri = f'mysql://pmourey:fifa2022@{hostname}/sample'
+# mysql://username:password@server/db
+
+app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.sqlite3'
+app.config['SECRET_KEY'] = "fifa 2022"
+
+db = SQLAlchemy(app)
+
+
+class Job(db.Model):
+    id = db.Column('job_id', db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    url = db.Column(db.String(100))
+    zipCode = db.Column(db.String(5))
+    company = db.Column(db.String(20))
+    contact = db.Column(db.String(20))
+
+    def __init__(self, name: str, url: str, zipCode: str, company: str, contact: str):
+        self.name = name
+        self.url = url
+        self.zipCode = zipCode
+        self.company = company
+        self.contact = contact
+
+
+@app.route('/')
+def show_all():
+    return render_template('show_all.html', jobs=Job.query.all())
+
+
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        if not request.form['name'] or not request.form['url'] or not request.form['company']:
+            flash('Please enter all the fields', 'error')
+        else:
+            job = Job(request.form['name'], request.form['url'],
+                      request.form['zipCode'], request.form['company'], request.form['contact'])
+
+            db.session.add(job)
+            db.session.commit()
+            flash('Record was successfully added')
+            return redirect(url_for('show_all'))
+    return render_template('new.html')
+
+
+@app.before_request
+def create_tables():
+    db.create_all()
+
+
+if __name__ == '__main__':
+    # db.create_all()
+    app.run(debug=True)
