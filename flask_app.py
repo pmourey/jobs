adc01b5edb61126e6580943794c3e7b7415833d3
Philippe Mourey <philippe@mourey.com>
2023-03-01 21:16:31 +0100

    add date feature

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
+import locale
+from datetime import datetime#, timezone
+from pytz import timezone
+
 from enum import Enum
 
 from flask import Flask, request, flash, url_for, redirect, render_template
 
 # https://pypi.org/project/flask/FLAS-sqlalchemy/
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import DateTime
 
 # SQLite
 # sqlite3_db = '/Users/display/Library/DBeaverData/workspace6/.metadata/sample-database-sqlite-1/Chinook.db'
@@ -19,6 +24,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///jobs.sqlite3'
 app.config['SECRET_KEY'] = "fifa 2022"
 
 db = SQLAlchemy(app)
+locale.setlocale(locale.LC_TIME, 'fr_FR')
 
 
 class Job(db.Model):
@@ -28,13 +34,15 @@ class Job(db.Model):
     zipCode = db.Column(db.String(5))
     company = db.Column(db.String(20))
     contact = db.Column(db.String(20))
+    date = db.Column(db.DateTime)
 
-    def __init__(self, name: str, url: str, zipCode: str, company: str, contact: str):
+    def __init__(self, name: str, url: str, zipCode: str, company: str, contact: str, date: DateTime):
         self.name = name
         self.url = url
         self.zipCode = zipCode
         self.company = company
         self.contact = contact
+        self.date = date
 
 
 @app.route('/')
@@ -44,12 +52,13 @@ def show_all():
 
 @app.route('/new', methods=['GET', 'POST'])
 def new():
+    paris = timezone('Europe/Paris')
     if request.method == 'POST':
         if not request.form['name'] or not request.form['url'] or not request.form['company']:
             flash('Please enter all the fields', 'error')
         else:
             job = Job(request.form['name'], request.form['url'],
-                      request.form['zipCode'], request.form['company'], request.form['contact'])
+                      request.form['zipCode'], request.form['company'], request.form['contact'], datetime.now(paris))
 
             db.session.add(job)
             db.session.commit()
