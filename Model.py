c4dbd0b4170a39d81a4db4cbe2946512dc7d6d0d
Philippe Mourey <philippe@mourey.com>
2023-11-19 18:46:18 +0100

    fixed bug for Job.user_id FK

--- Model.py
+++ Model.py
@@ -21,7 +21,7 @@ class Role(Enum):
     READER = 2
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
